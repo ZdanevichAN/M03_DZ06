94195fb748935bbf5f2291a1afc8c40d1466524c
Alecsander Zdanevich <alecsdjkzda@gmail.com>
2023-05-31 08:58:26 +0300

    ДЗ_31.05.23_Урок 6. Одномерные массивы. Продолжение

---
+++ task_41_DZ/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+Console.Clear();
+
+int Prompt(string message){//метод запроса ввода данных int от пользователя
+    int num = 0;
+        try{
+            Console.Write(message);
+            num = int.Parse(Console.ReadLine());
+        }
+        catch{Console.Write("\n \n Ошибка ввода данных метода Prompt");}
+    return num;
+}
+
+int[] GetUserNum(int size){//метод ввода пользователем чисел в массив int
+    int[] result = new int[size];
+        try{
+            for(int i=0;i<size;i++){
+                if(i+1!=3 || ((i+1)%10)!=3){result[i] = Prompt($"\n Введите {i+1}-ое число:  ");}
+                else{result[i] = Prompt($"\n Введите {i+1}-ье число:  ");}
+            }
+        }
+        catch{Console.WriteLine("\n \n Ошибка ввода переменных метода GetUserNum");}
+    return result;
+}
+
+void CountPositive(int[] array){//метод подсчета положительных чисел
+    int result=0;
+        try{
+            foreach(var el in array){
+                if(el>0){result++;}
+            }
+        }
+        catch{Console.WriteLine("\n \n Ошибка ввода данных методу CountPositive");}
+    Console.WriteLine($"\n В массиве [{String.Join(" | ",array)}] \n чисел  больше нуля: {result}");
+}
+
+int size = Prompt("Введите количество чисел для ввода: ");
+int[] array = GetUserNum(size);
+CountPositive(array);
---
+++ task_43_DZ/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+Console.Clear();
+double Prompt(string message){//метод ввода данных от пользователя double
+    double result = 0;
+        try{
+            Console.Write(message);
+            result = double.Parse(Console.ReadLine());
+        }
+        catch{Console.Write("Ошибка ввода данных методу Prompt");}
+    return result;
+}
+
+double[] GetStraight(double b1,double k1,double b2,double k2){//метод  пересечения прямых
+    double[] result = new double[2];double x=0;double y =0;
+        try{
+            result[0] = Math.Round((-(b1-b2)/(k1-k2)),2);
+            result[1] = Math.Round((k1*result[0]+b1),2);
+        }
+        catch{Console.WriteLine("\n Ошибка ввода данных методу GetStraight");}
+    return result;
+}
+
+double[] cross  = GetStraight(Prompt("\n для уравнения первой прямой (y = k1 * x + b1), введите b1: "),
+                           Prompt("\n для уравнения первой прямой (y = k1 * x + b1), введите k1: "),
+                           Prompt("\n для уравнения второй прямой (y = k2 * x + b2), введите b2: "),
+                           Prompt("\n для уравнения второй прямой (y = k2 * x + b2), введите k2: "));
+Console.WriteLine($"\n координаты пересечения: [{String.Join(" | ",cross)}]");
